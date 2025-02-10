731e3b666958b4896126eb86b5e1e81cb2f81263
G Manjunth <manjudba1222@gmail.com>
2025-02-10 16:53:47 +0530

    hi

---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "project" {
+
+}
+
+variable "env" {
+  
+}
+
+variable "sg_tags" {
+    default = {}
+  
+}
+
+variable "comman_tags" {
+    type =map
+  
+}
+
+variable "vpc_id" {
+  
+}
+
+variable "description" {
+  
+}
